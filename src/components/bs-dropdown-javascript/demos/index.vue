5a2137d60f12e3bf712d75ba2ee415ba2375fc6b
拾晚霞 <941477276@qq.com>
2023-11-08 21:03:48 +0800

    feat: 原生javascript模式增加右键菜单示例

--- src/components/bs-dropdown-javascript/demos/index.vue
+++ src/components/bs-dropdown-javascript/demos/index.vue
@@ -3,6 +3,8 @@
     <Basic></Basic>
     <Intelligent></Intelligent>
     <InDialog></InDialog>
+    <Contextmenu></Contextmenu>
+    <ContextmenuContextElement></ContextmenuContextElement>
   </div>
 </template>
 
@@ -11,13 +13,17 @@ import { defineComponent } from 'vue';
 import Basic from './basic.vue';
 import Intelligent from './intelligent.vue';
 import InDialog from './in-dialog.vue';
+import Contextmenu from './contextmenu.vue';
+import ContextmenuContextElement from './contextmenu-context-element.vue';
 
 export default defineComponent({
   name: 'DemoIndex',
   components: {
     Basic,
     Intelligent,
-    InDialog
+    InDialog,
+    Contextmenu,
+    ContextmenuContextElement
   }
 });
 </script>
